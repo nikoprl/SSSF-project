ca5a393d6fe60b12f5766c2e2c95ae373e628846
Niko <45226198+nikoprl@users.noreply.github.com>
2022-04-22 19:17:09 +0300

    Add register form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,10 +1,176 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Paper,
+  styled,
+  Typography,
+} from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#121212",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: "#FFF",
+}));
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  backgroundColor: purple[500],
+  "&:hover": {
+    backgroundColor: purple[700],
+  },
+}));
+
+const StyledOutlinedInput = styled(OutlinedInput)({
+  backgroundColor: "#33303e",
+});
+
 const RegisterForm = () => {
-    return (
-      <>
-        <p>Register form</p>
-      </>
-    );
+  const [values, setValues] = React.useState({
+    username: "",
+    password: "",
+    confirmpassw: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
   };
-  
-  export default RegisterForm;
-  
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Box sx={{ mt: 12, mb: 10 }}>
+        <Grid
+          container
+          spacing={3}
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+          columns={16}
+        >
+          <Grid item xs={7}>
+            <Item
+              sx={{ borderRadius: "16px", padding: "60px" }}
+              style={{ minHeight: "auto" }}
+            >
+              <Typography component="h4" variant="h4" gutterBottom>
+                Sign up
+              </Typography>
+              <form>
+                <Grid container direction="column">
+                  <FormControl sx={{ m: 1, width: "45ch" }} variant="outlined">
+                    <InputLabel
+                      sx={{ color: "white" }}
+                      htmlFor="outlined-adornment-username"
+                    >
+                      Username
+                    </InputLabel>
+                    <StyledOutlinedInput
+                      sx={{ color: "white" }}
+                      id="outlined-adornment-username"
+                      value={values.username}
+                      onChange={handleChange("username")}
+                      label="Username"
+                    />
+                  </FormControl>
+                  <FormControl sx={{ m: 1, width: "45ch" }} variant="outlined">
+                    <InputLabel
+                      sx={{ color: "white" }}
+                      htmlFor="outlined-adornment-password"
+                    >
+                      Password
+                    </InputLabel>
+                    <StyledOutlinedInput
+                      sx={{ color: "white" }}
+                      id="outlined-adornment-password"
+                      type={values.showPassword ? "text" : "password"}
+                      value={values.password}
+                      onChange={handleChange("password")}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            style={{ color: "white" }}
+                          >
+                            {values.showPassword ? (
+                              <VisibilityOff />
+                            ) : (
+                              <Visibility />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Password"
+                    />
+                  </FormControl>
+                  <FormControl sx={{ m: 1, width: "45ch" }} variant="outlined">
+                    <InputLabel
+                      sx={{ color: "white" }}
+                      htmlFor="outlined-adornment-password"
+                    >
+                      Confirm Password 
+                    </InputLabel>
+                    <StyledOutlinedInput
+                      sx={{ color: "white" }}
+                      id="outlined-adornment-password"
+                      type={values.showPassword ? "text" : "password"}
+                      value={values.confirmpassw}
+                      onChange={handleChange("confirmpassw")}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            style={{ color: "white" }}
+                          >
+                            {values.showPassword ? (
+                              <VisibilityOff />
+                            ) : (
+                              <Visibility />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Confirm password"
+                    />
+                  </FormControl>
+                  <ColorButton sx={{ mt: 5, mb: 2 }}>Submit</ColorButton>
+                </Grid>
+              </form>
+              <p>Already got account?</p> <Button component={Link} to="/login">Sign in!</Button>
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default RegisterForm;
