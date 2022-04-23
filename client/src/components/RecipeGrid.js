dcf80537777a3ad15cdd238dfea5fc03ea29fbb2
Niko <45226198+nikoprl@users.noreply.github.com>
2022-04-23 14:15:10 +0300

    Improve responsiveness

--- client/src/components/RecipeGrid.js
+++ client/src/components/RecipeGrid.js
@@ -1,13 +1,16 @@
-import { Box, Grid, Paper } from "@mui/material";
+import { AccessTimeOutlined } from "@mui/icons-material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Image from "mui-image";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "#121212",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(0),
   textAlign: "center",
   color: "#FFF",
+  display: "flex",
+  flexDirection: "column",
 }));
 
 const RecipeGrid = () => {
@@ -21,28 +24,136 @@ const RecipeGrid = () => {
           justifyContent="center"
           columns={16}
         >
-          <Grid item xs={7}>
-          <Item sx={{ borderRadius: "16px" }} style={{ minHeight: "600px" }}>
-              <Image src="placeholdr.png" height={250} margin="-10px" bgColor="grey" />
-              Recipe Placeholder
+          <Grid item xs={14} sm={13} md={7} lg={6}>
+            <Item
+              sx={{
+                borderRadius: "16px",
+                overflow: "hidden",
+                minHeight: "600px",
+                minWidth: "350px",
+                maxWidth: "900px",
+              }}
+            >
+              <Image
+                src=""
+                easing="linear"
+                fit="cover"
+                height={250}
+                bgColor="grey"
+              />
+              <Typography variant="h5" flexGrow={1}>
+                Recipe Name
+              </Typography>
+              <Box
+                textAlign="center"
+                display="inline-flex"
+                gap="5px"
+                alignItems="center"
+                alignSelf="flex-end"
+                margin="15px"
+              >
+                <AccessTimeOutlined />
+                <span>10 min</span>
+              </Box>
             </Item>
           </Grid>
-          <Grid item xs={7}>
-          <Item sx={{ borderRadius: "16px" }} style={{ minHeight: "600px" }}>
-              <Image src="placeholdr.png" height={250} bgColor="grey" />
-              Recipe Placeholder
+          <Grid item xs={14} sm={13} md={7} lg={6}>
+            <Item
+              sx={{
+                borderRadius: "16px",
+                overflow: "hidden",
+                minHeight: "600px",
+                minWidth: "350px",
+                maxWidth: "900px",
+              }}
+            >
+              <Image
+                src=""
+                easing="linear"
+                fit="cover"
+                height={250}
+                bgColor="grey"
+              />
+              <Typography variant="h5" flexGrow={1}>
+                Recipe Name
+              </Typography>
+              <Box
+                textAlign="center"
+                display="inline-flex"
+                gap="5px"
+                alignItems="center"
+                alignSelf="flex-end"
+                margin="15px"
+              >
+                <AccessTimeOutlined />
+                <span>10 min</span>
+              </Box>
             </Item>
           </Grid>
-          <Grid item xs={7}>
-            <Item sx={{ borderRadius: "16px" }} style={{ minHeight: "600px" }}>
-              <Image src="placeholdr.png" height={250} bgColor="grey" />
-              Recipe Placeholder
+          <Grid item xs={14} sm={13} md={7} lg={6}>
+            <Item
+              sx={{
+                borderRadius: "16px",
+                overflow: "hidden",
+                minHeight: "600px",
+                minWidth: "350px",
+                maxWidth: "900px",
+              }}
+            >
+              <Image
+                src=""
+                easing="linear"
+                fit="cover"
+                height={250}
+                bgColor="grey"
+              />
+              <Typography variant="h5" flexGrow={1}>
+                Recipe Name
+              </Typography>
+              <Box
+                textAlign="center"
+                display="inline-flex"
+                gap="5px"
+                alignItems="center"
+                alignSelf="flex-end"
+                margin="15px"
+              >
+                <AccessTimeOutlined />
+                <span>10 min</span>
+              </Box>
             </Item>
           </Grid>
-          <Grid item xs={7}>
-          <Item sx={{ borderRadius: "16px" }} style={{ minHeight: "600px" }}>
-              <Image src="placeholdr.png" height={250} bgColor="grey" />
-              Recipe Placeholder
+          <Grid item xs={14} sm={13} md={7} lg={6}>
+            <Item
+              sx={{
+                borderRadius: "16px",
+                overflow: "hidden",
+                minHeight: "600px",
+                minWidth: "350px",
+                maxWidth: "900px",
+              }}
+            >
+              <Image
+                src=""
+                easing="linear"
+                fit="cover"
+                height={250}
+                bgColor="grey"
+              />
+              <Typography variant="h5" flexGrow={1}>
+                Recipe Name
+              </Typography>
+              <Box
+                textAlign="center"
+                display="inline-flex"
+                gap="5px"
+                alignItems="center"
+                alignSelf="flex-end"
+                margin="15px"
+              >
+                <AccessTimeOutlined />
+                <span>10 min</span>
+              </Box>
             </Item>
           </Grid>
         </Grid>
