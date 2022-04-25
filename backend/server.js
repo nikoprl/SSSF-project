21ae420b064c34ec207237b0b1a570f12611ed07
Niko <45226198+nikoprl@users.noreply.github.com>
2022-04-25 19:42:59 +0300

    Apollo Server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,47 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import typeDefs from "./schemas/index";
+import resolvers from "./resolvers/index";
+import dotenv from "dotenv";
+import connectMongo from "./database/database";
+import { checkAuth } from "./utils/auth";
 
-const app = express()
+dotenv.config();
 
-app.get("/", function(req, res) {
-  res.send("Backend")
-})
+(async () => {
+  try {
+    const conn = await connectMongo();
+    if (conn) {
+      console.log("Connected successfully.");
+    } else {
+      throw new Error("Database not connected");
+    }
 
-app.listen(4000, (e) => {
-  console.log("\n \u001b[1;33m 🗄️  Backend listening on port 4000 \n" );
-})
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: async ({ req }) => {
+        if (req) {
+          const user = await checkAuth(req);
+          return { user, req };
+        }
+      },
+    });
+
+    const app = express();
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: process.env.PORT || 4000 }, () => {
+      console.log("DB URL=", process.env.DB_URL);
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      );
+    });
+  } catch (e) {   
+    console.log("server error: " + e.message);
+    console.log("Stack trace: " + e.stack);
+  }
+})();
